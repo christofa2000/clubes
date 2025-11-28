e75243a336b8b711962bf0c2a738b326960a33e9
christofa2000 <christofa2000@gmail.com>
2025-11-28 01:37:50 -0300

    Configurar aplicacion Next.js con TypeScript Tailwind y shadcn

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['../../packages/eslint-config', 'next/core-web-vitals'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  ignorePatterns: ['.next', 'node_modules']
+};
+
